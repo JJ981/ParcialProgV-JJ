08d70b233b69a6bb86ffbfd43e3ec0369675ecee
GAMER <GAMER@DESKTOP-SCA0ECD>
2024-06-04 10:53:57 -0300

    Agregar archivos de proyecto.

---
+++ Models/Proveedore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParcialProg.Models
+{
+    public partial class Proveedore
+    {
+        public int ProveedorId { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string RazonSocial { get; set; } = null!;
+        public string? Telefono { get; set; }
+        public string? Direccion { get; set; }
+        public string? Email { get; set; }
+        public string? ContactoPrincipal { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+        public string? Observaciones { get; set; }
+    }
+}
---
+++ Models/ProveedoresDataContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ParcialProg.Models
+{
+    public partial class ProveedoresDataContext : DbContext
+    {
+        public ProveedoresDataContext()
+        {
+        }
+
+        public ProveedoresDataContext(DbContextOptions<ProveedoresDataContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Proveedore> Proveedores { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=DESKTOP-SCA0ECD\\SQLEXPRESS; database=ProveedoresData; integrated security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Proveedore>(entity =>
+            {
+                entity.HasKey(e => e.ProveedorId)
+                    .HasName("PK__Proveedo__61266BB9E806B0A5");
+
+                entity.Property(e => e.ProveedorId).HasColumnName("ProveedorID");
+
+                entity.Property(e => e.ContactoPrincipal).HasMaxLength(100);
+
+                entity.Property(e => e.Direccion).HasMaxLength(255);
+
+                entity.Property(e => e.Email).HasMaxLength(100);
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("date")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Nombre).HasMaxLength(100);
+
+                entity.Property(e => e.RazonSocial).HasMaxLength(150);
+
+                entity.Property(e => e.Telefono).HasMaxLength(20);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
